7247f4310516c6b09417b6ae631806bca7d11e34
Павел Рогожкин <rogozhkinpaul@yandex.ru>
2022-10-04 15:33:06 +0300

    add models

---
+++ models/movie.js
@@ -0,0 +1,83 @@
+const mongoose = require('mongoose');
+const isURL = require('validator/lib/isURL');
+
+const movieSchema = new mongoose.Schema({
+
+    country: {
+        type: String,
+        required: true,
+    },
+
+    director: {
+        type: String,
+        required: true,
+    },
+    
+    duration: {
+        type: Number,
+        required: true,
+    },
+    
+    year: {
+        type: String,
+        required: true,
+    },
+    
+    description: {
+        type: String,
+        required: true,
+    },
+
+    image: {
+        type: String,
+        validate: {
+            validator: (v) => isURL(v),
+            message: 'Некорректный формат ссылки'
+        },
+        required: true,
+    },
+
+    trailerLink: {
+        type: String,
+        validate: {
+            validator: (v) => isURL(v),
+            message: 'Некорректный формат ссылки'
+        },
+        required: true,
+    },
+
+    thumbnail: {
+        type: String,
+        validate: {
+            validator: (v) => isURL(v),
+            message: 'Некорректный формат ссылки'
+        },
+        required: true,
+    },
+
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true,
+    },
+
+    movieId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+
+    nameRU: {
+        type: String,
+        required: true,
+    },
+
+    nameEN: {
+        type: String,
+        required: true,
+    },
+
+}, {
+    versionKey: false,
+});
+
+module.exports = mongoose.model('movie', movieSchema);
---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
+
+const userSchema = new mongoose.Schema({
+    
+    name: {
+        type: String,
+        minlength: 2,
+        maxlength: 30,
+        require: true,
+    },
+
+    email: {
+        type: String,
+        unique: true,
+        validate: {
+            validator: (v) => isEmail(v),
+            message: 'Неккоректный формат почты',
+        },
+        required: true,
+    },
+
+    password: {
+        type: String,
+        select: false,          // comment for debug
+        required: true,
+    },
+
+}, {
+    versionKey: false,
+    toObject: { useProjection: true },
+    toJSON: { useProjection: true },
+});
+
+module.exports = mongoose.model('user', userSchema);
