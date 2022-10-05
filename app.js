a4255f3d504a5f50a29d0fffcf1c3176a9178e92
Rogozhkin Pavel <rogozhkin_pa@rtsoft.ru>
2022-10-05 14:00:15 +0300

    add app require

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const console = require('console');
+const cors = require('cors');
