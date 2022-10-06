f84ba06cdf43244494bc9b06703fdbfe543f4af7
Rogozhkin Pavel <rogozhkin_pa@rtsoft.ru>
2022-10-06 17:25:07 +0300

    edit app; add routes and errors in app

--- app.js
+++ app.js
@@ -4,17 +4,63 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const console = require('console');
 const cors = require('cors');
+const helmet = require('helmet');
 
-const { celebrate, Joi, errors } = require('celebrate');
-const { createNewUser, login } = require('./controllers/users');
-const { auth } = require('./middlewares/auth');
-const { NotFoundError } = require('./errors/not-found-err');
+const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { errorsHandler } = require('./middlewares/errors');
+const { routes } = require('./routes/routes');
 
 require('dotenv').config();
 
 const {
-    PORT = 3000,
+    PORT = 4000,
     MONGO_URI = 'mongodb://localhost:27017/bitfilmsdb',
 } = process.env;
+
+const app = express();
+
+app.use(helmet());
+
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    }),
+);
+
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        // 'http://frontendURL',
+        'https://localhost:3000',
+        // 'https://frontendURL',
+    ],
+    credentials: true,
+}));
+
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+
+app.use(requestLogger);
+
+app.use(routes);
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use(errorsHandler);
+
+async function server() {
+    await mongoose.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: false,
+    });
+    await app.listen(PORT, () => {
+        console.log(`Server starting on port: ${PORT}`);
+    });
+};
+
+server();
+  
